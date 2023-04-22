c65a846df499f9c5d93019d028641281307ed1f5
Anarasty <murloc.ninja16@gmail.com>
2023-04-22 21:10:04 +0300

    data base upgrade + need fix searchbar results

--- product.js
+++ product.js
@@ -1,57 +1,3 @@
-//?GOOD WORK
-// import products from "./data.js";
-
-// const productList = document.getElementById("product-list");
-// const categoryList = document.getElementById("category-list");
-// const saleCheckbox = document.getElementById("sale");
-// const limitedCheckbox = document.getElementById("limited");
-
-// function displayProducts(products) {
-//   productList.innerHTML = "";
-//   for (let product of products) {
-//     const productDiv = document.createElement("div");
-//     productDiv.innerHTML = `<div class="product-card">
-//       <img src="${product.image}">
-//       <h2>${product.name}</h2>
-//       <p>${product.description}</p>
-//       <p>Price: $${product.price.toFixed(2)}</p>
-//     </div>`;
-//     productList.appendChild(productDiv);
-//   }
-// }
-
-// function filterProducts() {
-//   const selectedCategory = categoryList.querySelector(".selected").textContent;
-//   const showSale = saleCheckbox.checked;
-//   const showLimited = limitedCheckbox.checked;
-
-//   const filteredProducts = products.filter((product) => {
-//     if ((showSale && !product.onSale) || (showLimited && !product.isLimited)) {
-//       return false;
-//     }
-//     return selectedCategory === "All" || product.category === selectedCategory;
-//   });
-
-//   displayProducts(filteredProducts);
-// }
-
-// categoryList.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   if (event.target.nodeName === "A") {
-//     const selectedCategory = event.target.textContent;
-//     categoryList.querySelectorAll("a").forEach((link) => {
-//       link.classList.remove("selected");
-//     });
-//     event.target.classList.add("selected");
-//     filterProducts();
-//   }
-// });
-
-// saleCheckbox.addEventListener("change", filterProducts);
-// limitedCheckbox.addEventListener("change", filterProducts);
-
-// displayProducts(products);
-
 //!test4
 import products from "./data.js";
 
@@ -77,7 +23,7 @@ function displayProducts(products) {
         <p>$${product.price.toFixed(2)}</p>
         <p class="new-price-txt">${product.onSale === true ? "$"+product.newPrice : ""}</p>
       </div>
-      <p>${product.isLimited === true ? "Limited" : ""}</p>
+      <p class="limited-txt">${product.isLimited === true ? "Limited" : ""}</p>
       <a href="productCard.html?id=${
         product.id
       }" class="product-card-btn">Show product</a>
@@ -91,7 +37,7 @@ function resetFilters() {
   categoryList.querySelector("li:first-child a").classList.add("selected");
   saleCheckbox.checked = false;
   limitedCheckbox.checked = false;
-  select.value = "small-to-high";
+  select.value = "none";
   displayProducts(products);
   categoryList.querySelectorAll("a").forEach((link) => {
     link.classList.remove("selected");
@@ -157,13 +103,15 @@ searchInput.addEventListener("input", () => {
   });
 
   // Очищаем область с результатами поиска перед каждым новым поиском
-  searchResults.innerHTML = "block";
+  searchResults.innerHTML = "";
 
   filteredProducts.forEach((product) => {
     const productElement = document.createElement("div");
     productElement.innerHTML = `
+        <div class="search-card">
         <img src="${product.image}" alt="${product.name}">
         <p>${product.name}</p>
+        </div>
       `;
     productElement.addEventListener("click", () => {
       // При клике на элемент с результатом поиска переходим на карточку продукта
