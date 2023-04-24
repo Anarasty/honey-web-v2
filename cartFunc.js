c7d148eb84978cc04fca8fc21896e1036340fed4
Anarasty <murloc.ninja16@gmail.com>
2023-04-24 09:58:06 +0300

    product page styles

---
+++ cartFunc.js
@@ -0,0 +1,83 @@
+const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+
+function displayCartItems(items) {
+  const cartItemsContainer = document.getElementById("cartItemsContainer");
+  let cartItemsHTML = "";
+  let totalPrice = 0;
+
+  items.forEach((item) => {
+    const price = item.onSale ? item.newPrice : item.price;
+    const quantity = item.quantity || 1;
+    totalPrice += price * quantity;
+
+    cartItemsHTML += `
+      <div class="cart-item">
+        <img src="${item.image}" alt="${item.name}">
+        <div class="cart-item-info">
+          <h3>${item.name}</h3>
+          <p>Price: $${price.toFixed(2)}</p>
+          <div class="quantity">
+            <button class="minus-btn" data-id="${item.id}">-</button>
+            <input type="text" value="${quantity}" readonly>
+            <button class="plus-btn" data-id="${item.id}">+</button>
+            <button class="remove-btn" data-id="${item.id}">Remove</button>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  cartItemsContainer.innerHTML = cartItemsHTML;
+  document.getElementById("totalPrice").textContent = totalPrice.toFixed(2);
+
+  const deleteButtons = document.querySelectorAll(".remove-btn");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const itemId = parseInt(button.dataset.id);
+      const itemIndex = cartItems.findIndex((item) => item.id === itemId);
+      if (itemIndex !== -1) {
+        cartItems.splice(itemIndex, 1);
+        localStorage.setItem("cart", JSON.stringify(cartItems));
+        displayCartItems(cartItems);
+      }
+    });
+  });
+
+  // event listeners to plus and minus buttons
+  const plusBtns = document.querySelectorAll(".plus-btn");
+  plusBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const id = btn.getAttribute("data-id");
+      const item = items.find((item) => item.id == id);
+      item.quantity = (item.quantity || 1) + 1;
+      localStorage.setItem("cart", JSON.stringify(items));
+      updateCart();
+    });
+  });
+
+  const minusBtns = document.querySelectorAll(".minus-btn");
+  minusBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const id = btn.getAttribute("data-id");
+      const item = items.find((item) => item.id == id);
+      if (item.quantity > 1) {
+        item.quantity--;
+        localStorage.setItem("cart", JSON.stringify(items));
+        updateCart();
+      }
+    });
+  });
+
+  function updateCart() {
+    displayCartItems(JSON.parse(localStorage.getItem("cart")));
+  }
+}
+
+const checkoutButton = document.getElementById("checkoutButton");
+checkoutButton.addEventListener("click", () => {
+  // Redirect to checkout page
+  window.location.href = "/checkout.html";
+});
+
+displayCartItems(cartItems);
+
--- cartProduct.js
+++ cartProduct.js
@@ -1,39 +1,35 @@
 import products from "./data.js";
-      const productDetails = document.getElementById("productDetails");
+const productDetails = document.getElementById("productDetails");
 
-      function getProductById(id) {
-        return products.find((product) => product.id === id);
-      }
-      // <p>SALE! ${product.onSale === true ? "$"+product.newPrice : "NOT SALE"}</p>
-      function displayProduct(product) {
-        productDetails.innerHTML = `
+function getProductById(id) {
+  return products.find((product) => product.id === id);
+}
+// <p>SALE! ${product.onSale === true ? "$"+product.newPrice : "NOT SALE"}</p>
+function displayProduct(product) {
+  productDetails.innerHTML = `
           <div class="product-details">
             <img src="${product.image}">
             <div class="product-info-section">
               <h2>${product.name}</h2>
               <p>${product.description}</p>
-              <p>Category: ${product.category}</p>
-            ${
+              <p class="category-txt">Category: ${product.category}</p>
+            <div class="price-box">${
               product.onSale === true
                 ? `<p>Price: <span style="text-decoration: line-through;">$${product.price.toFixed(
                     2
-                  )}</span></p><p>SALE! $${product.newPrice.toFixed(2)}</p>`
+                  )}</span></p><p class="sale-paragraph">Sale price: $${product.newPrice.toFixed(2)}</p>`
                 : `<p>Price: $${product.price.toFixed(2)}</p>`
-            }
+            }</div>
             <a href="" class="add-to-cart-btn">Add to cart</a>
             </div>
           </div>
         `;
-      }
+}
 
-      const urlParams = new URLSearchParams(window.location.search);
-      const productId = parseInt(urlParams.get("id"));
-      const product = getProductById(productId);
-      displayProduct(product);
-
-
-
-
+const urlParams = new URLSearchParams(window.location.search);
+const productId = parseInt(urlParams.get("id"));
+const product = getProductById(productId);
+displayProduct(product);
 
 const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
 
@@ -49,4 +45,3 @@ const addToCartButton = document.querySelector(".add-to-cart-btn");
 addToCartButton.addEventListener("click", () => {
   addToCart(product);
 });
-
